83de2f4a7ef79f5c02e649dd9974305787837f13
drix_man <endrissuofe@gmail.com>
2025-07-18 14:46:58 +0100

    Initial setup with Express app, tests, ESLint, and GitHub Actions CI workflow

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('<h1>Welcome to My CI/CD YAML Demo</h1><p>This is a simple static page served by Express.</p>');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
+module.exports = app;
